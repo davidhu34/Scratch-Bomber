c2bcb443c17c521316fc1aab2fdfea32112ac286
davidhu34 <b01901133@gmail.com>
2017-04-05 11:20:34 +0800

    meeting prod

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log('Listening at http://localhost:3000/');
+app.use(express.static(__dirname + '/public'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join( __dirname, 'prod.html' ));
 });
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});	
+
+var server = http.createServer( app )
